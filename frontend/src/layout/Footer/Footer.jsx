e456a54582f6d0b5a119f62a2bf73d9f26ff1235
Naoh <27915369+N40h@users.noreply.github.com>
2024-04-12 15:51:05 +0200

    Ajout du composant de Coordonnees et du style par default, refacto du formulaire de contact

--- frontend/src/layout/Footer/Footer.jsx
+++ frontend/src/layout/Footer/Footer.jsx
@@ -1,9 +1,12 @@
 import ContactForm from "../../components/ContactForm/ContactForm"
+import Coordonnees from "../../components/Coordonnees/Coordonnees"
+import "./Footer.scss";
 
 export default function Footer() {
     return (
         <footer className="footer">
             <ContactForm />
+            <Coordonnees />
         </footer>
     )
 };
