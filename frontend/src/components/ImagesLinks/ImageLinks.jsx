85a7e389407b968015ea76f98145ba6e039a343f
Naoh <27915369+N40h@users.noreply.github.com>
2024-04-15 16:45:52 +0200

    Fix styling issues in ContactSection, ImageLinks, and Header components

--- frontend/src/components/ImagesLinks/ImageLinks.jsx
+++ frontend/src/components/ImagesLinks/ImageLinks.jsx
@@ -6,8 +6,8 @@ export default function ImageLinks({sectionClass, titles, links, src, alt}) {
   return (
     <section className={sectionClass}>
         {titles.map((title, index) => (
-            <div className={`${sectionClass}__content`}>
-                <a key={index} className={`${sectionClass}__link`} href={links[index]}>
+            <div className={`${sectionClass}__content`} key={index}>
+                <a className={`${sectionClass}__link`} href={links[index]}>
                     <h3 className={`${sectionClass}__title`}>{title}</h3>
                     <img className={`${sectionClass}__image`} src={src[index]} alt={alt[index]} />
                 </a>
