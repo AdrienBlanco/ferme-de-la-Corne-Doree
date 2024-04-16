e19799ff1e0df973559b28b988a9dd2a6494b0aa
Naoh <27915369+N40h@users.noreply.github.com>
2024-04-16 13:48:00 +0200

    Ajout du style pour la section de présentation

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
 
             <TextAndImage
                 sectionClass="textandimage"
-                title="Presentation"
+                title="Qui sommes nous ?"
                 text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur dignissimos culpa quisquam corrupti voluptatibus aut tempore recusandae quas beatae sequi iure, odit officiis facere saepe ipsam ratione ut voluptas soluta.
                 Exercitationem aspernatur aliquid distinctio sunt officiis architecto, voluptatum fugiat eius pariatur saepe soluta dolore asperiores fugit veniam cupiditate rerum culpa numquam inventore harum adipisci, facere minima quam. Quos, excepturi mollitia.
                 Quod, consequatur natus nemo veniam explicabo quae dolorum cum ab autem ea ad quidem fugit! Aliquam neque tempore voluptates similique accusamus corrupti, ut voluptatibus officia quod expedita. Consequatur, quia sint?"
