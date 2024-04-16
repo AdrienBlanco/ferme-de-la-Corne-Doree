d67c7f3d159b9963998d6714436cf92204e30051
Naoh <27915369+N40h@users.noreply.github.com>
2024-04-16 17:30:00 +0200

    Ajout du background wave a la section ImageLinks

--- frontend/src/components/ImagesLinks/ImageLinks.jsx
+++ frontend/src/components/ImagesLinks/ImageLinks.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import "./ImageLinks.scss";
 
-export default function ImageLinks({sectionClass, titles, links, src, alt}) {
+export default function ImageLinks({ sectionClass, wave, titles, links, src, alt }) {
+  const waveSection = `${sectionClass} ${wave ? `waveBg` : ''}`;
+
   return (
-    <section className={sectionClass}>
+    <section className={waveSection}>
         {titles.map((title, index) => (
             <div className={`${sectionClass}__content`} key={index}>
                 <a className={`${sectionClass}__link`} href={links[index]}>
