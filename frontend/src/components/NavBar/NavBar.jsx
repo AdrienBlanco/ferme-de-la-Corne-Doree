f6b3c9fcbb1c8ce90823cadb0d685c039595a9da
AdrienBlanco <adrienblanco89@gmail.com>
2024-04-16 16:41:25 +0200

    correction link dans Navbar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,5 @@
-import { Link } from "react-router-dom"
-
 export default function NavBar({name, path, target}) {
     return (
-        <li><Link to={path} target={target}>{name}</Link></li>
+        <li><a href={path} target={target}>{name}</a></li>
     )
 };
