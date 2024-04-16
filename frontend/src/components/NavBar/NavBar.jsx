ed6bb12ef2982db7f0aa4e679e3e149b8f2e195c
AdrienBlanco <adrienblanco89@gmail.com>
2024-04-16 14:49:57 +0200

    layout

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom"
+
 export default function NavBar({name, path}) {
     return (
-        <li><a href={path}>{name}</a></li>
+        <li><Link to={path}>{name}</Link></li>
     )
 };
