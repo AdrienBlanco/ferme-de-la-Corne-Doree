8aeed02645a6fcc9482b706e35f1f10fb8159c48
Naoh <27915369+N40h@users.noreply.github.com>
2024-04-17 16:25:53 +0200

    Refactor Location component and Slideshow component, and update Footer component layout

--- frontend/src/layout/Footer/Footer.jsx
+++ frontend/src/layout/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import "./Footer.scss";
 export default function Footer() {
     return (
         <footer className="footer">
-            <p>
+            <div className="footer__content">
             Conception :<div className="margin-logo"><span className="margin-logo--M">m</span><span className="margin-logo--A">a</span>RGIN</div> Web Solutions
-            </p>
+            </div>
         </footer>
     )
 };
