e0c592dbf90d54777c40bc467d55272557be0214
Naoh <27915369+N40h@users.noreply.github.com>
2024-04-22 13:52:29 +0200

    Ajout de l'animation pour le chevron sur l'input select

--- frontend/src/components/FormInputs/Select/Select.jsx
+++ frontend/src/components/FormInputs/Select/Select.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-export default function Select({label, name, value, handleChange, placeholder, required, options}) {
+export default function Select({ label, name, value, handleChange, placeholder, required, options }) {
+    const selectRef = useRef();
+
+    useEffect(() => {
+        const handleFocus = () => {
+            selectRef.current.parentElement.classList.add('open');
+            selectRef.current.parentElement.classList.remove('close');
+        }
+        const handleBlur = () => {
+            selectRef.current.parentElement.classList.remove('open');
+            selectRef.current.parentElement.classList.add('close');
+        }
+
+        const selectElement = selectRef.current;
+        selectElement.addEventListener('focus', handleFocus);
+        selectElement.addEventListener('blur', handleBlur);
+
+        return () => {
+            selectElement.removeEventListener('focus', handleFocus);
+            selectElement.removeEventListener('blur', handleBlur);
+        }
+    }, []);
+
     return (
     <>
         <label htmlFor={name}>{label}</label>
-        <select name={name} id={name} value={value} onChange={handleChange} required={required}>
-            <option value=''>{placeholder}</option>
-            {options.map(option => (
-                <option key={option} value={option}>{option}</option>
-            ))}
-        </select>
+        <div className='select-wrapper'>        
+            <select ref={selectRef} name={name} id={name} value={value} onChange={handleChange} required={required}>
+                <option value=''>{placeholder}</option>
+                {options.map(option => (
+                    <option key={option} value={option}>{option}</option>
+                ))}
+            </select>
+        </div>
     </>
   )
 }
